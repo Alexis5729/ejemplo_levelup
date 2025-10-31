8915a6479058fb3b0bd74942a7aa719872283c9c
Alexis5729 <al.pobletel@duocuc.cl>
2025-10-31 20:41:29 -0300

    Colores nuevos

--- app/src/main/java/com/example/ejemplo_level_up/ui/theme/Color.kt
+++ app/src/main/java/com/example/ejemplo_level_up/ui/theme/Color.kt
@@ -3,13 +3,12 @@ package com.example.ejemplo_level_up.ui.theme
 import androidx.compose.ui.graphics.Color
 
 
-val NeonBlue   = Color(0xFF00BFFF)
-val NeonGreen  = Color(0xFF39FF14)
-val NeonCyan   = Color(0xFF16E5FF)
-val OutlineNeon= Color(0xFF1E90FF)
-
-val DarkBg      = Color(0xFF000000)
-val DarkSurface = Color(0xFF0A0A0A)
-val SurfaceDim  = Color(0xFF111111)
-val TextPrimary = Color(0xFFFFFFFF)
-val TextSecondary = Color(0xFFCCCCCC)
+val NeonBlue = Color(0xFF00BFFF)
+val NeonGreen = Color(0xFF00FF9C)
+val NeonCyan = Color(0xFF00FFFF)
+val DarkBg = Color(0xFF0A0A0F)
+val DarkSurface = Color(0xFF15151F)
+val SurfaceDim = Color(0xFF1F1F2A)
+val TextPrimary = Color(0xFFEAEAEA)
+val TextSecondary = Color(0xFF9CA3AF)
+val OutlineNeon = Color(0xFF007BFF)
